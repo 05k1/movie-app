c2d244df3e5470109a6818ca160f2166027fe1e1
Tran Hau <hautran.01022002@gmail.com>
2024-07-26 14:58:05 +0700

    Build app movie trailer

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect } from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import { useState } from "react";
+
+function App() {
+  const [movie, setMovie] = useState([]);
+  const [movieRate, setMovieRate] = useState([]);
+  const [movieSearch, setMovieSearch] = useState([]);
+
+  const handleSearch = async (value) => {
+    setMovieSearch([]);
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?query=${value}&include_adult=false&language=vi&page=1`;
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+      const searchMovie = await fetch(url, options);
+      const data = await searchMovie.json();
+      setMovieSearch(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+
+      const url1 =
+        "https://api.themoviedb.org/3/movie/popular?language=vi&page=1";
+      const url2 =
+        "https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1";
+      const [res1, res2] = await Promise.all([
+        fetch(url1, options),
+        fetch(url2, options),
+      ]);
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+      setMovie(data1.results);
+      setMovieRate(data2.results);
+    };
+    fetchMovie();
+  }, []);
+
+  return (
+    <>
+      <div className="bg-black pb-10">
+        <Header onSearch={handleSearch} />
+        <Banner />
+        <MovieList title={"Phim Hot"} data={movie} />
+        <MovieList title={"Phim Đề Cử"} data={movieRate} />
+      </div>
+    </>
+  );
+}
+
+export default App;
